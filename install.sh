a9cc8fd75c5cdb6e313bae92267f17abd560cfe3
chameleonhash <chameleonhash@gmail.com>
2023-04-14 11:06:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,36 @@
-1 #!/bin/bash
- 2 # Original install script for Findsploit by @xer0dayz
- 3 # mod4termux by @chameleonhash
- 4
- 5
- 6 FINDSPLOIT_INSTALL_DIR=$PREFIX/share/findsploit                                       7                                                                                       8 OKBLUE='\033[94m'
- 9 OKRED='\033[91m'
-10 OKGREEN='\033[92m'
-11 OKORANGE='\033[93m'
-12 RESET='\e[0m'                                                                        13 REGEX='^[0-9]+$'
-14                                                                                      15 echo -e "$OKRED    _____           __           __      _ __"
-16 echo -e "$OKRED   / __(_)___  ____/ /________  / /___  (_) /_"                       17 echo -e "$OKRED  / /_/ / __ \/ __  / ___/ __ \/ / __ \/ / __/"
-18 echo -e "$OKRED / __/ / / / / /_/ (__  ) /_/ / / /_/ / / /_"                         19 echo -e "$OKRED/_/ /_/_/ /_/\__,_/____/ .___/_/\____/_/\__/"
-20 echo -e "$OKRED                      /_/"
-21 echo -e "$OKRED                 ______ __"
-22 echo -e "$OKRED  __ _  ___  ___/ / / // /____ ______ _  __ ____ __"
-23 echo -e "$OKRED /  ' \/ _ \/ _  /_  _/ __/ -_) __/  ' \/ // /\ \ /"
-24 echo -e "$OKRED/_/_/_/\___/\_,_/ /_/ \__/\__/_/ /_/_/_/\_,_//_\_\ "
-25 echo -e "$RESET"
-26 echo -e "$RESET"
-27 echo -e "$OKBLUE[*]$RESET Installing findsploit_mod4termux under $FINDSPLOIT_INSTALL>
-28 cp -Rf $PWD/* $FINDSPLOIT_INSTALL_DIR
-29 cd $FINDSPLOIT_INSTALL_DIR
-30 git clone https://gitlab.com/exploit-database/exploitdb.git $FINDSPLOIT_INSTALL_DIR/>
-31 chmod +x $FINDSPLOIT_INSTALL_DIR/findsploit
-32 chmod +x $FINDSPLOIT_INSTALL_DIR/copysploit
-33 chmod +x $FINDSPLOIT_INSTALL_DIR/compilesploit
-34 ln -s $PREFIX/share/findsploit/findsploit $PREFIX/bin/findsploit
-35 ln -s $PREFIX/share/findsploit/copysploit $PREFIX/bin/copysploit
-36 ln -s $PREFIX/share/findsploit/compilesploit $PREFIX/bin/compilesploit
-37 echo -e "$OKBLUE[*]$RESET Done! $RESET"
-38 echo -e "$OKRED[>]$RESET To run, type 'findsploit'! $RESET
+GNU nano 7.2                           install.sh
+#!/bin/bash
+# Original install script for Findsploit by @xer0dayz
+# mod4termux by @chameleonhash
+
+
+FINDSPLOIT_INSTALL_DIR=$PREFIX/share/findsploit                                                                                                                                 OKBLUE='\033[94m'
+OKRED='\033[91m'
+OKGREEN='\033[92m'
+OKORANGE='\033[93m'
+RESET='\e[0m'                                                                           
+REGEX='^[0-9]+$'
+echo -e "$OKRED    _____           __           __      _ __"
+echo -e "$OKRED   / __(_)___  ____/ /________  / /___  (_) /_"
+echo -e "$OKRED  / /_/ / __ \/ __  / ___/ __ \/ / __ \/ / __/"
+echo -e "$OKRED / __/ / / / / /_/ (__  ) /_/ / / /_/ / / /_"                            
+echo -e "$OKRED/_/ /_/_/ /_/\__,_/____/ .___/_/\____/_/\__/"
+echo -e "$OKRED                      /_/"
+echo -e "$OKRED                 ______ __"
+echo -e "$OKRED  __ _  ___  ___/ / / // /____ ______ _  __ ____ __"
+echo -e "$OKRED /  ' \/ _ \/ _  /_  _/ __/ -_) __/  ' \/ // /\ \ /"
+echo -e "$OKRED/_/_/_/\___/\_,_/ /_/ \__/\__/_/ /_/_/_/\_,_//_\_\ "
+echo -e "$RESET"
+echo -e "$RESET"
+echo -e "$OKBLUE[*]$RESET Installing findsploit_mod4termux under $FINDSPLOIT_INSTALL_DI>
+cp -Rf $PWD/* $FINDSPLOIT_INSTALL_DIR
+cd $FINDSPLOIT_INSTALL_DIR
+git clone https://gitlab.com/exploit-database/exploitdb.git $FINDSPLOIT_INSTALL_DIR/exp>
+chmod +x $FINDSPLOIT_INSTALL_DIR/findsploit
+chmod +x $FINDSPLOIT_INSTALL_DIR/copysploit
+chmod +x $FINDSPLOIT_INSTALL_DIR/compilesploit
+ln -s $PREFIX/share/findsploit/findsploit $PREFIX/bin/findsploit
+ln -s $PREFIX/share/findsploit/copysploit $PREFIX/bin/copysploit
+ln -s $PREFIX/share/findsploit/compilesploit $PREFIX/bin/compilesploit
+echo -e "$OKBLUE[*]$RESET Done! $RESET"
+echo -e "$OKRED[>]$RESET To run, type 'findsploit'! $RESET"
